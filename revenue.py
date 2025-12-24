b3e37bbb0f3ae7a8d4240e88ba95443234992bc5
wehars-0413 <kuriharay0106@gmail.com>
2025-12-24 16:51:57 +0900

    [FIX] csv出力へ対応

--- revenue.py
+++ revenue.py
@@ -1,4 +1,7 @@
 import sys
+import os
+import csv
+from datetime import datetime
 
 ########################################################################
 #メルカリ出品前に利益と利益率を計算するツール
@@ -7,8 +10,24 @@ import sys
 #例python revenue.py 5000 3000 500
 ########################################################################
 
+#利益の計算を行う関数
 def calc_profit(price,cost_price, shipping, fee_rate):
   return price - cost_price - shipping - ( price * fee_rate)
+
+#csvファイルへの書き込みを行う関数
+def export_result_csv(data: dict):
+  #outputディレクトリがなければ作成
+  os.makedirs("output", exist_ok=True)
+  #ファイル名生成
+  timestamp = datetime.now().strftime("%Y%m%d_%H%M%S")
+  filename = f"output/{timestamp}.csv"
+
+  with open(filename, mode="w", newline="", encoding="utf-8") as f:
+    write = csv.DictWriter(f, fieldnames=data.keys())
+    write.writeheader()
+    write.writerow(data)
+  print(f"CSV出力完了:{filename}")
+  return
 
 def main():
   #コマンドラインの引数を取得(配列の0番目は実行ファイル名になる)
@@ -17,27 +36,56 @@ def main():
 
   if len(args) == 4:
     try:
+      #1つ目の引数を価格
       price = int(args[1])
+      #2つ目の引数を原価
       cost_price = int(args[2])
+      #3つ目の引数を送料
       shipping = int(args[3])
+      #利益の計算
       profit = int(calc_profit(price, cost_price, shipping, fee_rate))
+      #利益率(原価基準)
+      #小数点以下を切り捨ての為int型へ
+      profit_b = int(profit / cost_price * 100)
+      #利益率(売価基準)
+      #小数点以下を切り捨ての為int型へ
+      profit_s = int(profit / price * 100)
+      #判定用
+      judge = "未判定"
 
       print("価格:", price)
       print("原価:", cost_price)
       print("送料:", shipping)
       print("手数料:", int(price * fee_rate))
       print("利益:", profit)
-      if cost_price  == 0:
-        print("利益率(原価基準):", int(profit / cost_price * 100),"%")
+      if cost_price  > 0:
+        print("利益率(原価基準):", profit_b,"%")
       else:
         print("利益率(原価基準):計算不可")
-      print("利益率(売価基準):", int(profit / price * 100),"%")
+      print("利益率(売価基準):",  profit_s,"%")
+
       if profit < 0:
-        print("赤字です")
+        judge = "赤字です"
       elif profit < 300:
-        print("利益が少なめです。(要塞検討)")
+        judge = "利益が少なめです。(要塞検討)"
       else:
-        print("出品候補です")
+        judge = "出品候補です"
+      print("判定:",judge)
+
+    #上記出力結果をファイルに書き込む
+      data = {
+        "価格": price,
+        "原価": cost_price,
+        "送料": shipping,
+        "手数料": int(price * fee_rate),
+        "利益": profit,
+        "利益率(原価基準)": profit_b,
+        "利益率(売価基準)": profit_s,
+        "判定": judge,
+        "日時": datetime.now().strftime("%Y-%m-%d_%H:%M:%S"),
+      }
+      #csvファイルへの書き込み
+      export_result_csv(data)
 
     except ValueError:
       print("数値を入力してください")
